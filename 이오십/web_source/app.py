40934bf4c132dbad5f750b9dc2cc10fd6909439a
SonSeungMin <sonsmmm@gmail.com>
2024-11-06 20:47:45 +0900

    ignore 동현

--- 이오십/web_source/app.py
+++ 이오십/web_source/app.py
@@ -11,8 +11,10 @@ def home():
     client_ip = request.remote_addr  # 요청한 클라이언트의 IP 주소 확인
     if client_ip == "192.168.0.6":
         return render_template('admin.html')  # 손승민 관리자 페이지
-    elif client_ip == "192.168.0.4":
-        return render_template('admin.html')  # 김동현 관리자 페이지
+        
+    # elif client_ip == "192.168.0.4":
+    #     return render_template('admin.html')  # 김동현 관리자 페이지
+
     else:
         return render_template('index.html')  # 다른 IP에서 접근 시 게스트 페이지
 
