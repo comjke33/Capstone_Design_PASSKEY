655bde6353249d5fe6966bf0829ac8b14901cc94
Kim001104 <dongsky2000@naver.com>
2024-11-07 18:39:21 +0900

    main.html에서 술래와 생존자가 지정된 타임이 끝나고 나오면 각각의 페이지로 리디렉션되게 함.

--- 이오십/web_source/app.py
+++ 이오십/web_source/app.py
@@ -1,25 +1,37 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+
 # def index():
 #     return render_template('index.html')  # 'templates/index.html' 파일을 렌더링합니다.
 
 def home():
-    client_ip = request.remote_addr  # 요청한 클라이언트의 IP 주소 확인
+    client_ip = request.remote_addr  # 요청한 클라이언트의 IP주소 확인
     if client_ip == "192.168.0.4":
         return render_template('index.html')  # 김동현 관리자 페이지
         
     elif client_ip == "192.168.0.3":
-        return render_template('seeker.html')   #술래 페이지
-    
+        target_page = url_for('seeker')        
     elif client_ip == "192.168.0.8":
-        return render_template("survivor1.html")    #생존자1 페이지
-
+        target_page = url_for('survivor1')  
     else:
-        return render_template('index.html')  # 다른 IP에서 접근 시 게스트 페이지
+        return render_template('index.html')
+    
+    # main.html로 렌더링하고 타겟 페이지 정보 전달
+    return render_template('main.html', target_page=target_page)
+
+# 각 HTML 파일에 대한 별도의 라우트를 설정하여 templates 폴더 내 파일을 렌더링
+@app.route('/seeker')
+def seeker():
+    return render_template('seeker.html')
+
+@app.route('/survivor1')
+def survivor1():
+    return render_template('survivor1.html')
+
 
 
 if __name__ == "__main__":
@@ -29,5 +41,4 @@ if __name__ == "__main__":
     key_path = os.path.join(base_dir, 'static', 'key.pem')
     
     ssl_context = (cert_path, key_path)
-    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context)
-
+    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context, debug=True)
---
+++ 이오십/web_source/static/main.js
@@ -0,0 +1,16 @@
+let countdown = 10;
+const timeElement = document.getElementById("time");
+
+// 카운트다운 타이머
+const timer = setInterval(() => {
+    if (countdown > 1) {
+        timeElement.textContent = `${countdown}초 뒤에 게임을 시작합니다.`;
+        countdown--;
+    } else {
+        timeElement.textContent = "1초 뒤에 게임을 시작합니다.";
+        clearInterval(timer);
+        setTimeout(() => {
+            window.location.href = targetPage; // 카운트다운 후 target_page로 리디렉션
+        }, 1000);
+    }
+}, 1000); // 1초마다 카운트다운
