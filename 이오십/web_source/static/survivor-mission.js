496326066761cf48a6a24fece6b9b0aa8fd468a3
Kim001104 <dongsky2000@naver.com>
2024-11-10 20:15:37 +0900

    미션 페이지 각각의 변수 하나씩 만듦

--- 이오십/web_source/static/survivor-mission.js
+++ 이오십/web_source/static/survivor-mission.js
@@ -1,2 +1,14 @@
 //생존자 1,2,3의 미션 코드
 /*각각의 생존자는 미션 지역에 들어왔을 경우 랜덤하게 버튼을 클릭해야하는 것이 출력됨.*/
+/*버튼을 누르면 playing.html로 넘어갈 수 있게*/
+
+function descend_timer(){
+    
+    const timer = document.getElementById("timer");         //타이머 감소 버튼 정의 
+    const occupation_button = doucument.getElementById("occupation_button");        //점령페이지 넘어가는 버튼 정의
+    const playing_button = document.getElementById("playing_button");               //플레잉페이지 넘어가는 버튼 정의
+
+
+    
+}
+
